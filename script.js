4eac9955b4474af7da7b49d2db38251feb8d4417
chandan maurya <76623223+chandan766@users.noreply.github.com>
2025-06-15 13:59:07 +0530

    Update script.js
    Updated the stun server url

--- script.js
+++ script.js
@@ -264,7 +264,10 @@ function createPeerConnection() {
       { urls: 'stun:stun.l.google.com:19302' },
       { urls: 'stun:stun1.l.google.com:19302' },
       { urls: 'stun:stun2.l.google.com:19302' },
-    ]
+      { urls: 'stun:stun3.l.google.com:19302' },
+      { urls: 'stun:stun4.l.google.com:19302' },
+    ], 
+    iceCandidatePoolSize: 2 
   });
   pc.oniceconnectionstatechange = () => {
     console.log('ICE connection state:', pc.iceConnectionState);
