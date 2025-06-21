5be5afd0b55a8c5718c4ba8dc04250985e238ac4
chandan maurya <76623223+chandan766@users.noreply.github.com>
2025-06-21 13:11:44 +0530

    Update script.js
    added new stun server - original js

--- script.js
+++ script.js
@@ -377,7 +377,8 @@ function createPeerConnection() {
     iceServers: [
       { urls: 'stun:stun.l.google.com:19302' },
       { urls: 'stun:stun1.l.google.com:19302' },
-     // { urls: 'stun:stun2.l.google.com:19302' },
+      { urls: 'stun:stun2.l.google.com:19302' },
+      { urls: 'stun:stun.stunprotocol.org:3478' },
       //{ urls: 'stun:stun3.l.google.com:19302' },
       //{ urls: 'stun:stun4.l.google.com:19302' },
     ], 
